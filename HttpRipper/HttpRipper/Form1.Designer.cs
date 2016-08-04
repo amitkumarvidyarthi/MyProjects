1a0a4b27c31bceecd919dd9e0cea5c8b3c974b2a
AmitVidyarthi <amitkumarvidyarthi@gmail.com>
2016-08-04 17:53:16 +0530

    Add files via upload

---
+++ HttpRipper/HttpRipper/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace HttpRipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(22, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(361, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "http://www.tutorialspoint.com/csharp/";
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGo.Location = new System.Drawing.Point(389, 12);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 1;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(22, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(442, 316);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 369);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ HttpRipper/HttpRipper/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HttpRipper
+{
+    public partial class Form1 : Form
+    {
+        public string rootPath = @"D:\temp\";
+        private List<string> downloadedPaths = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            Download(textBox1.Text, rootPath);
+        }
+
+        public void Download(string url, string localpath)
+        {
+            downloadedPaths.Add(url);
+
+            WebClient client = new WebClient();
+            client.Proxy = WebRequest.DefaultWebProxy;
+            client.Credentials = System.Net.CredentialCache.DefaultCredentials; ;
+            client.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            string filename = Path.GetFileName(url);
+            if (string.IsNullOrEmpty(filename))
+            {
+                string[] split = url.Split('/');
+                filename = split[split.Count() - 1];
+                if (string.IsNullOrEmpty(filename) && (split.Count() >1))
+                {
+                    filename = split[split.Count() - 2];
+                }
+            }
+
+            if (string.IsNullOrEmpty( filename ))
+            {
+                return;
+            }
+
+            string localfile = Path.Combine(rootPath, filename);
+            try
+            {
+                client.DownloadFile(url, localfile);
+            }
+            catch
+            {
+                return;
+            }
+
+            List<string> subPaths = new List<string>();
+
+
+            using (StreamReader sr = new StreamReader(localfile))
+            {
+                string buffer = sr.ReadToEnd();
+                Regex reg = new Regex("href=\"[^\"]*");
+                MatchCollection matches = reg.Matches(buffer);
+                foreach (Match match in matches)
+                {
+                    if (!url.Equals(match.Value))
+                    {
+                        if (!subPaths.Contains(match.Value))
+                        {
+                            subPaths.Add(match.Value);
+                        }
+                    }
+                }
+            }
+            listBox1.DataSource = downloadedPaths;
+            //mkdir filename
+             foreach (string subPath in subPaths)
+            {
+                string[] split = subPath.Split('\"');
+                string fullUrl;
+                if (split[1].StartsWith("/"))
+                {
+                    fullUrl = url + split[1];
+                }
+                else
+                {
+                    fullUrl = url;
+                }
+
+                if (!downloadedPaths.Contains(split[1]))
+                {
+                    Download(split[1], Path.Combine(localpath, filename));
+                }
+            }
+             
+        }
+    }
+}
